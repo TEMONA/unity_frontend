7dda1d770a3e302d0d89456b518977cc503c0fb7
temona-kichikichi <kawashita@temona.co.jp>
2022-12-29 09:41:13 +0900

    lint系の設定内容調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier',
-  ],
-  plugins: [],
-  // add your custom rules here
-  rules: {},
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
+	plugins: ['prettier', 'vue'],
+	rules: {
+		'no-console': 'warn',
+		indent: [2, 'tab'],
+		'vue/no-v-html': 'off',
+		'no-fallthrough': 'off',
+		'space-before-function-paren': 'off',
+		'node/handle-callback-err': 'off',
+		'node/no-new-require': 'off',
+		'node/no-path-concat': 'off',
+		'array-bracket-spacing': ['error', 'never'],
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				trailingComma: 'all',
+				useTabs: true,
+			},
+		],
+	},
 }
