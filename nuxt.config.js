7157f2d7e57805ac5bd1b596293a6e659f604a31
temona-kichikichi <kawashita@temona.co.jp>
2022-12-30 15:29:36 +0900

    scssのフォルダ名をいい感じに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: ['~/assets/scss/foundation/_index.scss'],
+	css: ['~/assets/scss/foundations/_index.scss'],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [],
