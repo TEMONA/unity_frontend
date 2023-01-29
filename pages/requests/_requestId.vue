2ccbd9bd65f89c5cbf601c02c655dd858b70cfff
temona-kichikichi <kawashita@temona.co.jp>
2023-01-29 15:27:18 +0900

    /requests/_requestId実装

---
+++ pages/requests/_requestId.vue
@@ -0,0 +1,98 @@
+<template>
+	<v-row class="request">
+		<v-col cols="12" md="4" class="request__sidebar">
+			<v-avatar size="auto" width="100%" class="mb-2">
+				<v-img :aspect-ratio="1" :src="user.image" alt="社員画像" />
+			</v-avatar>
+			<div class="text-center">
+				<Heading :level="2" :text="user.name" class="font-weight-medium" />
+				<Paragraph
+					:text="`${user.department}事業部 ${user.group}グループ ${user.role}`"
+					class="mb-1"
+				/>
+				<Paragraph :text="user.chatworkId" class="grey--text" />
+				<v-row>
+					<v-col cols="6">
+						<v-btn outlined block color="primary">チャットを開く</v-btn>
+					</v-col>
+					<v-col cols="6">
+						<v-btn outlined block color="primary">プロフィールを見る</v-btn>
+					</v-col>
+					<v-col v-if="request.direction === 'from'" cols="12">
+						<v-btn block color="primary">日時を選択して申請を承認する</v-btn>
+					</v-col>
+					<v-col v-if="request.direction === 'to'" cols="6">
+						<v-btn block color="primary">申請を編集する</v-btn>
+					</v-col>
+					<v-col v-if="request.direction === 'to'" cols="6">
+						<v-btn block color="error">申請を削除する</v-btn>
+					</v-col>
+				</v-row>
+			</div>
+		</v-col>
+		<v-col cols="12" md="8">
+			<v-chip-group class="mb-3">
+				<v-chip v-for="tag in user.tags" :key="tag">
+					{{ tag }}
+				</v-chip>
+			</v-chip-group>
+
+			<Paragraph
+				:text="`この人${
+					request.direction === 'from' ? 'から' : 'へ'
+				}送られた　${request.status}のリクエスト`"
+				class="text-h6"
+			/>
+
+			<v-row>
+				<v-col cols="12">
+					<v-card>
+						<v-card-title text="依頼詳細" />
+
+						<v-card-text v-text="request.detail" />
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-col>
+	</v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+	head() {
+		return {
+			title: '社員詳細',
+		}
+	},
+	data() {
+		return {
+			user: {
+				image: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
+				name: 'User Name',
+				department: '〇〇',
+				group: '〇〇',
+				role: 'グループ長',
+				chatworkId: '@chatwork_id',
+				tags: ['エンジニア職', '勤続3年', 'Aグレード'],
+			},
+			request: {
+				direction: 'from',
+				status: 'status',
+				detail:
+					'Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.',
+			},
+		}
+	},
+})
+</script>
+
+<style lang="scss">
+.user {
+	&__sidebar {
+		position: sticky;
+		top: calc(64px + 16px);
+	}
+}
+</style>
