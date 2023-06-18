44d20d280cc5c787f3f7e3229773b575643eda0f
temona-kichikichi <kawashita@temona.co.jp>
2023-06-18 13:46:37 +0900

    プロフィール画像のサイズを調整

--- components/organisms/UserOverview.vue
+++ components/organisms/UserOverview.vue
@@ -1,10 +1,12 @@
 <template>
-	<div>
-		<v-avatar v-if="image" size="auto" width="100%" class="mb-2">
-			<v-img :aspect-ratio="1" :src="image" alt="社員画像" />
-		</v-avatar>
+	<v-row>
+		<v-col cols="8" offset="2" class="mb-2">
+			<v-avatar v-if="image" size="auto" width="100%">
+				<v-img :aspect-ratio="1" :src="image" alt="社員画像" />
+			</v-avatar>
+		</v-col>
 
-		<div class="text-center">
+		<v-col cols="12" class="text-center">
 			<Paragraph :text="nameKana" class="text-caption mb-0" />
 			<Heading :level="2" :text="name" class="font-weight-medium mb-1" />
 			<Paragraph
@@ -16,8 +18,8 @@
 				:text="`@${chatworkId}`"
 				class="grey--text"
 			/>
-		</div>
-	</div>
+		</v-col>
+	</v-row>
 </template>
 
 <script lang="ts">
