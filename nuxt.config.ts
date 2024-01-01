667931bdc1530fb2c786d277dd667c28647cdbd0
temona-kichikichi <kawashita@temona.co.jp>
2024-01-01 21:42:52 +0900

    metaタグ情報を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      baseURL: '', // 環境変数の NUXT_PUBLIC_BASE_URL で設定
+    },
+  },
+  app: {
+    head: {
+      titleTemplate: '%s - テモナ社内SNS「Unity」',
+      title: 'unity_frontend',
+      htmlAttrs: {
+        lang: 'ja',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'robots', content: 'noindex, nofollow' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+});
