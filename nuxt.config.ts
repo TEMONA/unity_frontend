4478a5ac1fdb0d91f4806f7cc47b3f709ffbbb35
temona-kichikichi <kawashita@temona.co.jp>
2024-01-02 14:15:00 +0900

    headからhidを削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { name: 'description', content: '' },
         { name: 'format-detection', content: 'telephone=no' },
         { name: 'robots', content: 'noindex, nofollow' },
       ],
