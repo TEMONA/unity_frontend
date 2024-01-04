818895a9149eb5b4a9255341387341e2ec288acc
temona-kichikichi <kawashita@temona.co.jp>
2024-01-04 12:42:29 +0900

    yarn add @pinia/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,47 +2,48 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  runtimeConfig: {
-    googleClientId: '',
-    googleApiKey: '',
-    public: {
-      baseURL: '',
-      apiUrl: '',
-    },
-  },
-  app: {
-    head: {
-      titleTemplate: '%s - テモナ社内SNS「Unity」',
-      title: 'unity_frontend',
-      htmlAttrs: {
-        lang: 'ja',
-      },
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: '' },
-        { name: 'format-detection', content: 'telephone=no' },
-        { name: 'robots', content: 'noindex, nofollow' },
-      ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    },
-  },
-  build: {
-    transpile: ['vuetify'],
-  },
-  hooks: {
-    'vite:extendConfig': (config) => {
-      config.plugins!.push(vuetify());
-    },
-  },
-  vite: {
-    ssr: {
-      noExternal: ['vuetify'],
-    },
-    define: {
-      'process.env.DEBUG': false,
-    },
-  },
-  css: ['@/assets/main.scss'],
+	devtools: { enabled: true },
+	runtimeConfig: {
+		googleClientId: '',
+		googleApiKey: '',
+		public: {
+			baseURL: '',
+			apiUrl: '',
+		},
+	},
+	app: {
+		head: {
+			titleTemplate: '%s - テモナ社内SNS「Unity」',
+			title: 'unity_frontend',
+			htmlAttrs: {
+				lang: 'ja',
+			},
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ name: 'description', content: '' },
+				{ name: 'format-detection', content: 'telephone=no' },
+				{ name: 'robots', content: 'noindex, nofollow' },
+			],
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		},
+	},
+	modules: ['@pinia/nuxt'],
+	build: {
+		transpile: ['vuetify'],
+	},
+	hooks: {
+		'vite:extendConfig': (config) => {
+			config.plugins!.push(vuetify());
+		},
+	},
+	vite: {
+		ssr: {
+			noExternal: ['vuetify'],
+		},
+		define: {
+			'process.env.DEBUG': false,
+		},
+	},
+	css: ['@/assets/main.scss'],
 });
---
+++ store/snackbar.ts
@@ -0,0 +1,43 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+
+const colors = {
+	error: '#A04747',
+	success: '#23A39C',
+};
+
+interface Payload {
+	status: number;
+	messageText?: string;
+}
+
+interface StatusCodeMap {
+	[key: number]: string;
+}
+
+const statusCodeMap: StatusCodeMap = {
+	200: '',
+	400: 'パラメータが不正です',
+	401: 'ログイン情報が正しくありません',
+	403: '閲覧権限がありません',
+	404: 'ページが見つかりません',
+	500: '時間をおいて再度お試しください',
+};
+
+export const useSnackbarStore = defineStore('snackbar', () => {
+	const isShow = ref(false);
+	const message = ref('');
+	const color = ref(colors.error);
+
+	function displaySnackbar({ status, messageText = '' }: Payload) {
+		isShow.value = true;
+		message.value = messageText || statusCodeMap[status] || '';
+		color.value = status === 200 ? colors.success : colors.error;
+	}
+
+	function closeSnackbar() {
+		isShow.value = false;
+	}
+
+	return { isShow, message, color, displaySnackbar, closeSnackbar };
+});
